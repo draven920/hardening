c39624427dd506c2dd6c45772cd5a826cce871fe
draven920 <34629513+draven920@users.noreply.github.com>
2022-01-11 14:40:22 +0700

    Update hardening.sh

--- hardening.sh
+++ hardening.sh
@@ -3,14 +3,14 @@
 # Date:    09.01.2016
 # Version:    16.9.1
 
-echo -e "Server Updating : " > /home/user/.hardening.log
-sudo apt-get update -yy >> /home/user/.hardening.log
+echo -e "Server Updating : "  > /home/.hardening.log
+sudo apt-get update -yy >> /home/.hardening.log
 
 echo -e "Delete Telenet"
-sudo apt remove telnet -yy >> /home/user/.hardening.log
+sudo apt remove telnet -yy >> /home/.hardening.log
 
 echo -e "Delete Aplikasi openoffice* thumb& "
-sudo apt remove openoffice* && sudo apt remove thumb -yy >> /home/user/.hardening.log
+sudo apt remove openoffice* && sudo apt remove thumb -yy >> /home/.hardening.log
 
 echo -e "Downloading Rkhunter"
 wget https://telkomuniversity.dl.sourceforge.net/project/rkhunter/rkhunter/1.4.6/rkhunter-1.4.6.tar.gz
@@ -20,7 +20,7 @@ tar -xvf rkhunter-1.4.6.tar.gz
 cd rkhunter-1.4.6/
 
 echo -e "Install Rkhunter & Run"
-./installer.sh --install >> /home/user/.hardening.log
+./installer.sh --install >> /home/.hardening.log
 rkhunter --check
 
 echo -e "Hardening Selesai"
