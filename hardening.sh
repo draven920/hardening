6f021805b537927592d3738d60690522b7aca1ea
draven920 <34629513+draven920@users.noreply.github.com>
2022-01-11 15:44:58 +0700

    Update hardening.sh

--- hardening.sh
+++ hardening.sh
@@ -8,21 +8,43 @@ sudo apt-cache update >> /home/.hardening.log
 sudo apt-cache upgrade -y >> /home/.hardening.log
 
 
-echo -e "Delete Telenet"
+echo -e "Delete Telenet" >> /home/.hardening.log
 sudo apt remove telnet -yy >> /home/.hardening.log
 
-echo -e "Delete Aplikasi openoffice* thumb& "
+echo -e "Delete Aplikasi openoffice* thumb " >> /home/.hardening.log
 sudo apt remove openoffice* && sudo apt remove thumb -yy >> /home/.hardening.log
 
-# echo -e "Downloading Rkhunter"
-# wget https://telkomuniversity.dl.sourceforge.net/project/rkhunter/rkhunter/1.4.6/rkhunter-1.4.6.tar.gz
+echo -e "Downloading Rkhunter"
+wget https://telkomuniversity.dl.sourceforge.net/project/rkhunter/rkhunter/1.4.6/rkhunter-1.4.6.tar.gz
 
-# echo -e "Ekstrak Rkhunter"
-# tar -xvf rkhunter-1.4.6.tar.gz
-# cd rkhunter-1.4.6/
+echo -e "Ekstrak Rkhunter"
+tar -xvf rkhunter-1.4.6.tar.gz
+cd rkhunter-1.4.6/
 
-# echo -e "Install Rkhunter & Run"
-# ./installer.sh --install >> /home/.hardening.log
-# rkhunter --check
+echo -e "Install Rkhunter & Run"
+./installer.sh --install >> /home/.hardening.log
+rkhunter --check  >> /home/.hardening.log
 
-echo -e "Hardening Selesai."
+
+echo -e "Install CSF "  >> /home/.hardening.log
+cd /usr/src
+rm -fv csf.tgz
+wget https://download.configserver.com/csf.tgz
+tar -xzf csf.tgz
+cd csf
+sh install.sh
+
+echo -e "Matikan IP V6"  >> /home/.hardening.log
+
+printf "#Konfigurasi mematikan IP V6\nnet.ipv6.conf.all.disable_ipv6 = 1\nnet.ipv6.conf.default.disable_ipv6 = 1\nnet.ipv6.conf.lo.disable_ipv6 = 1
+" >> /etc/sysctl.conf
+sysctl -p >> /home/.hardening.log
+
+echo -e "Disable ftpd & sshd"  >> /home/.hardening.log
+service ftpd stop
+service sshd stop
+
+echo -e "Cek Services sshd"  >> /home/.hardening.log
+service sshd status   >> /home/.hardening.log
+
+echo -e "Hardening Selesai"
