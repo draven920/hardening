8fe7718b9e1097d4eca4528dfacda7201c3f4ae8
draven920 <34629513+draven920@users.noreply.github.com>
2022-01-12 12:49:17 +0700

    Update hardening.sh

--- hardening.sh
+++ hardening.sh
@@ -3,41 +3,41 @@
 # Date:    09.01.2016
 # Version:    16.9.1
 
-echo -e "Server Updating : "  #> /home/.hardening.log
+echo -e "\nServer Updating : "  #> /home/.hardening.log
 sudo apt-get update 
 sudo apt-get upgrade -yy 
 
-read -n 1 -s -r -p "Press any key to continue\n"
+read -n 1 -s -r -p "Press any key to continue"
 
 
-echo -e "Delete Telenet" #>> /home/.hardening.log
+echo -e "\nDelete Telenet" #>> /home/.hardening.log
 sudo apt remove telnet -yy
 
-read -n 1 -s -r -p "Press any key to continue\n"
+read -n 1 -s -r -p "Press any key to continue"
 
-echo -e "Delete Aplikasi openoffice* thumb " #>> /home/.hardening.log
+echo -e "\nDelete Aplikasi openoffice* thumb " #>> /home/.hardening.log
 sudo apt remove openoffice* && sudo apt remove thumb -yy 
 
-read -n 1 -s -r -p "Press any key to continue\n"
+read -n 1 -s -r -p "Press any key to continue"
 
-echo -e "Downloading Rkhunter"
+echo -e "\nDownloading Rkhunter"
 wget https://telkomuniversity.dl.sourceforge.net/project/rkhunter/rkhunter/1.4.6/rkhunter-1.4.6.tar.gz
 
-echo -e "Ekstrak Rkhunter"
+echo -e "\nEkstrak Rkhunter"
 tar -xvf rkhunter-1.4.6.tar.gz
 cd rkhunter-1.4.6/
 
-echo -e "Install Rkhunter & Run"
+echo -e "\nInstall Rkhunter & Run"
 ./installer.sh --install
 
-read -n 1 -s -r -p "Press any key to continue\n"
+read -n 1 -s -r -p "Press any key to continue"
 
 rkhunter --check 
 
-read -n 1 -s -r -p "Press any key to continue\n"
+read -n 1 -s -r -p "Press any key to continue"
 
 
-echo -e "Install CSF "  #>> /home/.hardening.log
+echo -e "\nInstall CSF "  #>> /home/.hardening.log
 cd /usr/src
 rm -fv csf.tgz
 wget https://download.configserver.com/csf.tgz
@@ -45,66 +45,66 @@ tar -xzf csf.tgz
 cd csf
 sh install.sh
 
-read -n 1 -s -r -p "Press any key to continue\n"
+read -n 1 -s -r -p "Press any key to continue"
 
-echo -e "Matikan IP V6"  #>> /home/.hardening.log
+echo -e "\nMatikan IP V6"  #>> /home/.hardening.log
 
 printf "#Konfigurasi mematikan IP V6\nnet.ipv6.conf.all.disable_ipv6 = 1\nnet.ipv6.conf.default.disable_ipv6 = 1\nnet.ipv6.conf.lo.disable_ipv6 = 1
 " >> /etc/sysctl.conf
 sysctl -p 
 cat /etc/sysctl.conf
 
-read -n 1 -s -r -p "Press any key to continue\n"
+read -n 1 -s -r -p "Press any key to continue"
 
-echo -e "Matikan CTRL + alt + Delete"  #>> /home/.hardening.log
+echo -e "\nMatikan CTRL + alt + Delete"  #>> /home/.hardening.log
 systemctl mask ctrl-alt-delete.target
 systemctl daemon-reload
 
-read -n 1 -s -r -p "Press any key to continue\n"
+read -n 1 -s -r -p "Press any key to continue"
 
-echo -e "Konfigurasi Standar Keamanan"  #>> /home/.hardening.log
+echo -e "\nKonfigurasi Standar Keamanan"  #>> /home/.hardening.log
 printf "#Konfigurasi standar keamanan partisi\ntmpfs /tmp tmpfs nosuid,noexec,nodev,rw 0 0\ntmpfs /var/tmp tmpfs nosuid,noexec,nodev,rw 0 0\ntmpfs /dev/shm tmpfs nosuid,noexec,nodev,rw 0 0\n
 " >> /etc/fstab
 cat /etc/fstab
 
-read -n 1 -s -r -p "Press any key to continue\n"
+read -n 1 -s -r -p "Press any key to continue"
 
-echo -e "Hapus X Windows (Mode GUI) "  #>> /home/.hardening.log
+echo -e "\nHapus X Windows (Mode GUI) "  #>> /home/.hardening.log
 printf "id:3:initdefault:" >> /etc/inittab
 cat /etc/inittab
 
-read -n 1 -s -r -p "Press any key to continue\n"
+read -n 1 -s -r -p "Press any key to continue"
 
 
-echo -e "Disable Service"  #>> /home/.hardening.log
+echo -e "\nDisable Service"  #>> /home/.hardening.log
 netstat -pnltu
 
-read -n 1 -s -r -p "Press any key to continue\n"
+read -n 1 -s -r -p "Press any key to continue"
 
 
 
-echo -e "Services SSHD Status"
-service sshd status
+# echo -e "\nServices SSHD Status"
+# service sshd status
 
-read -n 1 -s -r -p "Press any key to continue\n"
+# read -n 1 -s -r -p "Press any key to continue"
 
-echo -e "Folder Privillages"
+echo -e "\nFolder Privillages"
 cd /
 ls -la
 
-read -n 1 -s -r -p "Press any key to continue\n"
+read -n 1 -s -r -p "Press any key to continue"
 
-echo -e "Folder Privillages"
+echo -e "\nFolder Privillages"
 cat /etc/passwd
 
-read -n 1 -s -r -p "Press any key to continue\n"
+read -n 1 -s -r -p "Press any key to continue"
 
 cd ~
 rm hardening.sh
 rm -r rkhunter-1.4.6
 rm rkhunter-1.4.6.tar.gz
 
-echo -e "Hardening Selesai"
+echo -e "\nHardening Selesai"
 
 
 
